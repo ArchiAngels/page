daecac69dab948737af0c66579ac68f442553928
ArchiAngels <mikita.valynchyts@gmail.com>
2020-06-18 01:46:38 +0200

    finish_nav_bar_effect

--- main.js
+++ main.js
@@ -30,20 +30,33 @@ const btn_ul = {
         }
 }
 let ul_info = {
-    bool:true,
-    last_item:[],
+    current_anim:[],
+    can_anim:[],
     const:[],
-    n0:0,
-    n1:0
+    n0:[],
+    n1:[],
+    reset:function(finish_number){
+        console.log('RESET OBJ',this.current_anim[0]);
+        this.const[finish_number].last_item.innerHTML = this.const[finish_number].const;
+        console.log(this.const[finish_number].last_item.innerHTML);
+        this.can_anim[finish_number] = true;
+        this.n0[finish_number] = 0;
+        this.n1[finish_number] = 0;
+        this.const[finish_number].last_item = 0;
+        this.current_anim[finish_number] = 'finish';
+    }
 }
-let arr_sym = ['%','$','/','@',';','!','+','#'];
-// let arr_sym = ['0','3','8','6'];
+// let arr_sym = ['%','$','/','@',';','!','+','#'];
+let arr_sym = ['诶','比','西','迪','伊','弗','吉','尺','艾','杰','开','勒','马','娜','哦','屁','吉','儿','丝','提','伊','维','维','克','艾','德'];
+// let arr_sym = ['💥','😾','👻','🌈','❄','☄','⚡','🌚','🌍','🎄','🎨','🎋','🍭','🍪','🍑','🦊','🐅','🤔','⚔','👑','📚','💰','🍍','🦋','🍁','🦆'];
 let tmp_a = window.scrollY > btn_ul.class[0].offsetHeight;
 const mouse_pos ={
     x:0,
     y:0
 }
 let dis_pers = 25;
+
+init();
 
 window.addEventListener('mousemove',function(event){
         if(event.clientX > avatar[0].offsetParent.parentElement.offsetLeft && event.clientX < avatar[0].offsetParent.parentElement.offsetLeft+ avatar[0].offsetParent.parentElement.offsetWidth &&
@@ -70,31 +83,35 @@ window.addEventListener('mousemove',function(event){
             }
         }
         if(btn_ul.class[0].innerHTML == 'Clouse'){
-            for( let i = 0; i < ul_nav_bar[0].children.length;i++){
-                if(event.target == ul_nav_bar[0].children[i] && ul_info.bool && event.target != ul_info.last_item ){
-                    // console.log(`ANIM for ${ul_nav_bar[0].children[i].innerHTML}`);
-                    console.log('LOH');
-                    ul_info.last_item = event.target;
-                        let mini_obj = {};
-                        mini_obj['Elem'] = ul_nav_bar[0].children[i].innerHTML;
-                        mini_obj['Num'] = i;
-                    ul_info.const.push(mini_obj);
-                    //     mini_obj['Elem'] = ul_nav_bar[0].children[i];
-                    //     mini_obj['Num'] = i;
-                    // ul_info.last_item.push(mini_obj);
-                    if(ul_info.const.length < 5) {}
-                    else{
-                        ul_info.const.shift();
+                for( let i = 0; i < ul_nav_bar[0].children.length;i++){
+                    if(event.target == ul_nav_bar[0].children[i]){
+                        // console.log('IN',event.target);
+                        // break;
+                        if(ul_info.can_anim[i]){
+                            if(event.target != ul_info.const[i].last_item){
+                                    ul_info.const[i]['last_item'] = event.target;
+                                    ul_info.current_anim.push(i);
+                                    console.log(`START :: ${ul_info.const[i].last_item} index:: ${ul_info.current_anim}`);
+                                // if(ul_info.const.length < 5) {}
+                                // else{
+                                //     ul_info.const.shift();
+                                // }
+                                    anim_for(ul_info.const[i]);
+                                break;
+                            }
+                        }
                     }
-                    // if(ul_info.last_item.length < 5) {}
                     // else{
-                    //     ul_info.last_item.shift();
+                        // if(i == ul_nav_bar[0].children.length -1 && ul_info.can_anim != true){
+                        //         // console.log('OutSide',event.target);
+                        //     // setTimeout(function(){
+                        //         console.log('THIS TRO RESET');
+                        //         ul_info.reset();
+                        //     // },150);
+                        //         break;
+                        // }
                     // }
-                    console.log(ul_info.const);
-                    anim_for(ul_info.last_item);
-                    break;
                 }
-            }
         }
         
 });
@@ -143,54 +160,65 @@ window.addEventListener('scroll',function(){
     }
 })
 function anim_for(item){
-    console.log('go',item);
-    if(ul_info.bool){
-        // ul_info.bool = false;
-        ul_info.n0 = 0;
-        ul_info.n1 = item.innerHTML.length;
-        help_first(ul_info.n0,ul_info.n1);
+    // console.log('go',item,item.index);
+    if(ul_info.can_anim){
+        ul_info.can_anim[item.index] = false;
+        ul_info.n0[item.index] = 0;
+        ul_info.n1[item.index] = item.last_item.innerHTML.length;
+        help_first(ul_info.n0[item.index],ul_info.n1[item.index],item.index);
     }
 }
-function help_first(start_num,finish_num){
-    ul_info.n0 = start_num;
-    console.log(start_num,finish_num);
+function help_first(start_num,finish_num,index){
+    // console.log('go2',start_num,finish_num,index);
+    ul_info.n0[index] = start_num;
+    // console.log(index,ul_info.n0[index],start_num,finish_num);
     if(start_num < finish_num){
-        // console.log(ul_info.last_item.innerHTML[start_num]);
-        // setTimeout(function(){
-            help_two();
-        // },60);
+        help_two(index);
     }
     else{
-        // console.log('NOW',);
-        // ul_info.bool = true;
         setTimeout(function(){
-            
-            ul_info.last_item.innerHTML = ul_info.const[0].Elem;
-
-            ul_info.const.shift();
-        },500);
+            console.log('FINISH',index);
+            ul_info.reset(index);
+        },150);
     }
 }
-function help_two(n = 0,l = arr_sym.length){
-    // console.log('HELPE',ul_info);
+function help_two(index,n = 0,l = arr_sym.length){
     if(n < l){
         let tmp_1,tmp_2,tmp_3,tmp_4;
-        tmp_1 = ul_info.last_item.innerHTML.slice(0,ul_info.n0);
-        tmp_2 = ul_info.last_item.innerHTML.slice(ul_info.n0+1,ul_info.last_item.innerHTML.length);
-        tmp_4 = (Math.round(Math.random()*(arr_sym.length-1)));
-        tmp_3 = tmp_1 + arr_sym[tmp_4] + tmp_2;
-        // console.log(tmp_3,tmp_4,arr_sym[tmp_4],ul_info.last_item.innerHTML,ul_info.const);
-        ul_info.last_item.innerHTML = tmp_3;
+            tmp_1 = ul_info.const[index].last_item.innerHTML.slice(0,ul_info.n0[index]);
+            tmp_2 = ul_info.const[index].last_item.innerHTML.slice(ul_info.n0[index]+1,ul_info.const[index].last_item.innerHTML.length);
+            tmp_4 = (Math.round(Math.random()*(arr_sym.length-1)));
+        if(ul_info.const[index].last_item.innerHTML[ul_info.n0[index]] != ' '){
+            tmp_3 = tmp_1 + arr_sym[tmp_4] + tmp_2;
+        }
+        else{
+            tmp_3 = tmp_1 + ' ' + tmp_2;
+        }
+        // console.log(n,l,tmp_3);
+            ul_info.const[index].last_item.innerHTML = tmp_3;
         setTimeout(function(){
-            help_two(n+1,l);
-        },20);
+            help_two(index,n+1,l);
+        },0);
     }
     else{
-        // console.log("Finish one Part");
         setTimeout(function(){
-            help_first(ul_info.n0+1,ul_info.n1);
-        },20);
+            // console.log('go',ul_info);
+            help_first(ul_info.n0[index]+1,ul_info.n1[index],index);
+        },0);
     }
+}
+function init(){
+        for( let i = 0; i < ul_nav_bar[0].children.length;i++){
+            let obj = {};
+                obj['index'] = i;
+                obj['const'] = ul_nav_bar[0].children[i].innerHTML;
+                // obj['last_item'] = ul_nav_bar[0].children[i];
+            ul_info.const.push(obj);
+            ul_info.can_anim.push(true);
+            ul_info.n0.push(0);
+            ul_info.n1.push(0);
+            ul_nav_bar[0].children[i].innerHTML = 'Secret';
+        }
 }
 // event.clientX > wrap_avatar[0].offsetParent.parentElement.offsetLeft && event.clientX < wrap_avatar[0].offsetParent.parentElement.offsetLeft+wrap_avatar[0].offsetParent.parentElement.offsetWidth &&
 // event.clientY > wrap_avatar[0].offsetParent.parentElement.offsetTop && event.clientY < wrap_avatar[0].offsetParent.parentElement.offsetTop+wrap_avatar[0].offsetParent.parentElement.offsetHeight
